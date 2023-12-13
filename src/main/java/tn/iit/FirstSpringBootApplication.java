bab49ccfd4d1b19b68b85c1a7b2404abe5c066a7
Iyed Zairi <iyed.zairi@gmail.com>
2023-12-13 20:24:22 +0100

    Modifiy app

--- src/main/java/tn/iit/FirstSpringBootApplication.java
+++ src/main/java/tn/iit/FirstSpringBootApplication.java
@@ -29,29 +29,10 @@ public class FirstSpringBootApplication implements CommandLineRunner {
     @Transactional // pour le commit
     @Override
     public void run(String... args) throws Exception {
-
-        Client c1 =new Client("111","Hatem","zouari","Sfax" );
-        BankAccount b1=new BankAccount(1000,c1);
-        BankAccount b2=new BankAccount(204444,c1);
-        Client c2= new Client("111","hatem","zouari","sousse");
-        clientDao.save(c1);
-        clientDao.save(c2);
-        bankAccountDao.save(b1);
-        bankAccountDao.save(b2);
 //        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 //        // Replace "yourPlaintextPassword" with the password you want to encrypt.
 //        String encryptedPassword = passwordEncoder.encode("admin");
 //        System.out.println(encryptedPassword);
-
-
-
-
-
-
-
-
-
-
     }
 
 }
